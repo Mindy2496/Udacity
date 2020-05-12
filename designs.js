e4abb42d3c066c06542d4c05a01afea2a968c415
Mindy2496 <61294706+Mindy2496@users.noreply.github.com>
2020-05-12 13:05:19 -0600

    Add files via upload
    updated files for final submission to Udacity for the Pixel Art Maker Project.

--- designs.js
+++ designs.js
@@ -1,34 +1,36 @@
 // Select color input
-const colorPicker = document.getElementById('colorPicker').value;
+const color = document.getElementById('colorPicker').value;
+
 // Select size input
 const height = document.getElementById('inputHeight').value;
 const width = document.getElementById('inputWidth').value;
-const sizePicker = document.getElementById('sizePicker');
-//const myTable = document.getElementById('pixelCanvas');
-// When size is submitted by the user, call makeGrid()
-sizePicker.addEventListener('submit', function(event){
-  event.preventDefault();
-  const height = document.querySelector('#inputHeight').value;
-  const width = document.querySelector('#inputWidth').value;
-  makeGrid(height, width);
-  //console.log("so far so good"); //testing purposes
-});
 
-function makeGrid (height, width) {
-  const myTable = document.getElementById('pixelCanvas');
-  //resets  myTable when Submit is clicked again
-  myTable.innerHTML = "";
-  //create table rows and columns
-  for (i = 1; i<=height; i++){
-    var tableRows = document.createElement('tr');
-    for (x = 1; x<=width; x++) {
-    var tableCells = document.createElement('td');
-    tableRows.appendChild(tableCells);
-    myTable.appendChild(tableRows);
-    //fills cells with color picked
-    tableCells.addEventListener('click', function(event){
-      tableCells.style.backgroundColor = colorPicker.value;
-    });
-      };
+//set SizePicker variable
+const sizePicker = document.getElementById('sizePicker');
+
+// When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const height = document.querySelector('#inputHeight').value;
+    const width = document.querySelector('#inputWidth').value;
+    makeGrid(height, width);
+});
+//Function that creates grid based on input
+function makeGrid(height, width) {
+    const myTable = document.getElementById('pixelCanvas');
+    //resets myTable when Submit is clicked again
+    myTable.innerHTML = "";
+    //create table rows and columns based on input
+    for (i = 1; i <= height; i++) {
+        var tableRows = document.createElement('tr');
+        for (x = 1; x <= width; x++) {
+            var tableCells = document.createElement('td');
+            tableRows.appendChild(tableCells);
+            myTable.appendChild(tableRows);
+            //fills in cell color
+            tableCells.addEventListener('click', function(event) {
+                event.target.style.backgroundColor = document.getElementById('colorPicker').value;
+            });
+        };
     };
 };
